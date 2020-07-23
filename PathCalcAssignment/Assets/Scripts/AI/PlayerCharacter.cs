b028ff0830a2c46db29b157e75c59570f8ba0d66
paulio <pdkm2000@hotmail.com>
2020-07-23 08:50:58 +0100

    SphereOverlap cast

--- PathCalcAssignment/Assets/Scripts/AI/PlayerCharacter.cs
+++ PathCalcAssignment/Assets/Scripts/AI/PlayerCharacter.cs
@@ -6,9 +6,25 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    [SerializeField]
+    float _visionDistance = 12f;
+
+    [SerializeField]
+    int _maxEnimies = 100;
+
     Enemy _currentNearestEnemy = null;
     private bool _hasEnemies;
     Dictionary<int, GameObject> _enemies = null;
+    private int _enemiesLayerMask;
+    private int _currentNearestEnemyId;
+
+    private Collider[] _hits;
+
+    private void Start()
+    {
+        _enemiesLayerMask = LayerMask.GetMask("Enemies");
+        _hits = new Collider[_maxEnimies];
+    }
 
     private void Awake()
     {
@@ -18,9 +34,64 @@ public class PlayerCharacter : MonoBehaviour
 
     private void Update()
     {
-        if (_hasEnemies)
+        //if (_hasEnemies)
         {
-            UpdateNearestPosition();
+            UpdateNearestPositionViaRayCast();
+        }
+    }
+
+    private void UpdateNearestPositionViaRayCast()
+    {
+        if (Physics.OverlapSphereNonAlloc(transform.position, _visionDistance, _hits, _enemiesLayerMask) > 0)
+        {
+            var nearestEnemy = GetNearestEnemy(_hits);
+            if (nearestEnemy != null)
+            {
+                var nearestEnemyId = nearestEnemy.GetInstanceID();
+                if (nearestEnemyId != _currentNearestEnemyId)
+                {
+                    if (_currentNearestEnemyId != 0)
+                    {
+                        _currentNearestEnemy.IsNearest = false;
+                    }
+
+                    _currentNearestEnemy = nearestEnemy;
+                    _currentNearestEnemyId = nearestEnemyId;
+                    _currentNearestEnemy.IsNearest = true;
+                }
+            }
+        }
+    }
+
+    private Enemy GetNearestEnemy(Collider[] hits)
+    {
+        var nearestIndex = -1;
+        var nearestDistance = float.PositiveInfinity;
+        var currentPosition = transform.position;
+        for (int i = 0; i < hits.Length; i++)
+        {
+            var hit = hits[i];
+            if (hit == null)
+            {
+                break;
+            }
+
+            var calcDistance = Vector3.Distance(hit.transform.position, currentPosition);
+            if (calcDistance < nearestDistance)
+            {
+                nearestDistance = calcDistance;
+                nearestIndex = i;
+            }
+        }
+
+        if (nearestIndex > -1)
+        {
+            // print($"Nearest ememy was {hits[nearestIndex].name}");
+            return hits[nearestIndex].gameObject.GetComponent<Enemy>();
+        }
+        else
+        {
+            return null;
         }
     }
 
